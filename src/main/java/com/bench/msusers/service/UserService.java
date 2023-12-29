884e165adc0a7326b4d30fd47750752e053a0413
gonzalo290378 <designanimation.inc@gmail.com>
2023-12-29 12:32:20 -0300

    feat: fix service interface

--- src/main/java/com/bench/msusers/service/UserService.java
+++ src/main/java/com/bench/msusers/service/UserService.java
@@ -1,9 +1,16 @@
 package com.bench.msusers.service;
 
 import com.bench.msusers.dto.UserResponseDTO;
+import com.bench.msusers.model.User;
+
+import java.util.List;
 
 public interface UserService {
     public UserResponseDTO findById(Long id);
 
+    public List<UserResponseDTO> findAll();
 
+    public User save(User user);
+
+    public void delete(Long id);
 }
