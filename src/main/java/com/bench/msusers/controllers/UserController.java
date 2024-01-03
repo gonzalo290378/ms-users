4bf7a87353c16594a21e9b4c4fdb11abef174f13
gonzalo290378 <designanimation.inc@gmail.com>
2024-01-03 17:25:50 -0300

    feat: configuration config server

--- src/main/java/com/bench/msusers/controllers/UserController.java
+++ src/main/java/com/bench/msusers/controllers/UserController.java
@@ -6,10 +6,15 @@ import com.bench.msusers.service.impl.UserServiceImpl;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -18,6 +23,13 @@ public class UserController {
 
     @Autowired
     private UserServiceImpl userServiceImpl;
+
+    @Autowired
+    private Environment environment;
+
+    //CONFIG-SERVER
+    @Value("${configuration.text}")
+    private String text;
 
     @GetMapping()
     public ResponseEntity<List<UserResponseDTO>> findAll() {
@@ -43,6 +55,19 @@ public class UserController {
         userServiceImpl.delete(id);
         log.info("Calling delete with {}", id);
         return ResponseEntity.ok("Successfully deleted");
+    }
+
+    @GetMapping("/get-config")
+    public ResponseEntity<?> getConfig(@Value("${server.port}") String port) {
+        log.info("getConfig {}" + " port: " + port);
+        Map<String, String> json = new HashMap<>();
+        json.put("text", text);
+        json.put("port", port);
+
+        if (environment.getActiveProfiles().length > 0 && environment.getActiveProfiles()[0].equals("dev")) {
+            json.put("env", environment.getActiveProfiles()[0]);
+        }
+        return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
     }
 
 }
