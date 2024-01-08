f3aade73a6596432b1a5645bbafc3060da194e1b
gonzalo290378 <designanimation.inc@gmail.com>
2024-01-08 19:20:15 -0300

    feat: fix exceptions

--- src/main/java/com/bench/msusers/exceptions/DniNotFoundException.java
+++ src/main/java/com/bench/msusers/exceptions/DniNotFoundException.java
@@ -1,11 +1,7 @@
 package com.bench.msusers.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
-public class DniNotFoundException extends RuntimeException{
-    public DniNotFoundException(String message){
+public class DniNotFoundException extends RuntimeException {
+    public DniNotFoundException(String message) {
         super(message);
     }
 }
