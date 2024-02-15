623c0eaa58d37741ddb38951bc126ed6e5669fa5
gonzalo290378 <designanimation.inc@gmail.com>
2024-02-15 18:00:48 -0300

    feat: RC 0.0.2 auth-server

--- src/main/java/com/bench/msusers/auth/SecurityConfig.java
+++ src/main/java/com/bench/msusers/auth/SecurityConfig.java
@@ -6,25 +6,17 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authHttp) -> authHttp
                         .requestMatchers(HttpMethod.GET, "/authorized").permitAll()
-                        //.requestMatchers(HttpMethod.GET, "/").hasAnyAuthority("SCOPE_read", "SCOPE_write")
-                        //.requestMatchers(HttpMethod.POST, "/").hasAuthority("SCOPE_write")
                         .requestMatchers(HttpMethod.GET, "/", "/static/**", "/index.html", "/api/v1/users/username/{username}").permitAll()
-                        //.requestMatchers(HttpMethod.GET, "/api/users/login", "/api/users/{username}", "/api/users/logout", "/api/customers", "/api/storages").authenticated()
-                        //.requestMatchers(HttpMethod.POST, "/api/customers", "/api/storages").authenticated()
-                        //.requestMatchers(HttpMethod.PUT, "/api/customers/{id}", "/api/storages/{id}").authenticated()
-                        //.requestMatchers(HttpMethod.DELETE, "/api/users/{id}", "/api/storages/{id}", "/api/customers/{id}").authenticated()
-                        .requestMatchers(HttpMethod.GET, "/").hasAnyAuthority("SCOPE_read", "SCOPE_write")
-                        //.requestMatchers(HttpMethod.POST, "/").hasAuthority("SCOPE_write")
-                        //.anyRequest().denyAll()
+                        .requestMatchers(HttpMethod.GET, "/").hasAnyAuthority("ADMIN", "USER")
                         .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -33,5 +25,4 @@ public class SecurityConfig {
                 .oauth2ResourceServer(resourceServer -> resourceServer.jwt(withDefaults()));
         return http.build();
     }
-
 }
